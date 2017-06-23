e212c2205781b580d292029668c626e5e95764ee
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-06-23 22:38:19 +1000

    create gulpfile to run browsersync, compile sass and run server

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+const sassPath = './app/scss/**/*.scss';
+const scriptPath = './app/javascript/app.js'
+const htmlPath = './app/index.html'
+
+// Compile scss in css
+gulp.task('sass-to-css', function () {
+  return gulp.src(sassPath)
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('reload', function {
+  browserSync.reload();
+});
+
+gulp.task('sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  });
+});
+
+// Watch for saved changes and reload browser
+gulp.task('watch', ['sync', 'sass-to-css'], function() {
+  gulp.watch(sassPath, ['sass-to-css']);
+  gulp.watch(scriptPath, ['reload']);
+  gulp.watch(htmlPath, ['reload']);
+});
